bd730880679a7d6d8677cd2328b5b91900fba90f
Stefan Lissia <stefan.emailacct@gmail.com>
2015-06-03 15:53:42 +0100

    bikes can be reported broken

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,7 +1,21 @@
 class Bike
 
+  attr_reader :broken
+
+  def initialize
+    @broken = false
+  end
+
   def working?
     true
+  end
+
+  def report_broken
+
+  end
+
+  def broken?
+    @broken = true
   end
 
 
--- spec/features/public_returns_bike_spec.rb
+++ spec/features/public_returns_bike_spec.rb
@@ -11,4 +11,12 @@ feature 'public returns a bike' do
     expect(station.capacity).to eq 50
   end
 
+  scenario 'bike can be reported broken when returned' do
+    station = DockingStation.new
+    bike = Bike.new
+    bike.report_broken
+    expect(bike).to be_broken
+    expect { station.dock bike }.not_to raise_error
+  end
+
 end
